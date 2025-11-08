482f1ee1330d589a971fe939accdf3cc3dd304c3
NebulaJinxed <games.ga303@gmail.com>
2025-11-08 16:44:11 +0100

    V1.0.4 completed

--- src/main/java/com/github/nebulajinxed/movablehud/MovableHudClient.java
+++ src/main/java/com/github/nebulajinxed/movablehud/MovableHudClient.java
@@ -9,8 +9,8 @@ public class MovableHudClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HudRegistry.register(new ExampleHud(), "test1");
-        HudRegistry.register(new Example2Hud(), "test2");
+//        HudRegistry.register(new ExampleHud(), "test1");
+//        HudRegistry.register(new Example2Hud(), "test2");
         Commands.register();
     }
 }
