21e6b0ee32e09000d70103859ea86c9ab0cf2a5e
NebulaJinxed <games.ga303@gmail.com>
2025-11-08 16:40:45 +0100

    V1.0.4 completed

--- src/main/java/com/github/nebulajinxed/movablehud/MovableHudClient.java
+++ src/main/java/com/github/nebulajinxed/movablehud/MovableHudClient.java
@@ -3,16 +3,14 @@ package com.github.nebulajinxed.movablehud;
 import com.github.nebulajinxed.movablehud.hudElements.Example2Hud;
 import com.github.nebulajinxed.movablehud.hudElements.ExampleHud;
 import net.fabricmc.api.ClientModInitializer;
-import com.github.nebulajinxed.movablehud.screen.MovableHudScreen;
 
 public class MovableHudClient implements ClientModInitializer {
     public static String MODID = "movablehud";
 
     @Override
     public void onInitializeClient() {
-//        HudRegistry.register(new ExampleHud(), "test1");
-//        HudRegistry.register(new Example2Hud(), "test2");
+        HudRegistry.register(new ExampleHud(), "test1");
+        HudRegistry.register(new Example2Hud(), "test2");
         Commands.register();
-        MovableHudScreen.loadHudData();
     }
 }
